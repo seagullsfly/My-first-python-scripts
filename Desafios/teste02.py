70bc77d5ebbf0b0b14280a128f3d2f8c1c41d524
seagullsfly <42094711+seagullsfly@users.noreply.github.com>
2018-09-18 11:39:11 -0300

    Add files via upload

---
+++ Desafios/teste02.py
@@ -0,0 +1,6 @@
+Dia = input ('Qual dia você nasceu? ')
+Mes = input ('Qual mês você nasceu? ')
+Ano = input ('Qual ano você nasceu? ')
+print ('Você nasceu no dia ' + Dia + ' de ' + Mes + ' de ' + Ano + '. Correto?')
+
+       
