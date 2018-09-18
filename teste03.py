4ce86efb1ac4cc7484c871e335507be87bb8275e
seagullsfly <42094711+seagullsfly@users.noreply.github.com>
2018-09-18 11:27:07 -0300

    Delete teste03.py

--- teste03.py
+++
@@ -1,6 +0,0 @@
-Dia = input ('Qual dia você nasceu? ')
-Mes = input ('Qual mês você nasceu? ')
-Ano = input ('Qual ano você nasceu? ')
-print ('Você nasceu no dia ' + Dia + ' de ' + Mes + ' de ' + Ano + '. Correto?')
-
-       
